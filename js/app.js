ed031b6640a5f83f53f9f806ed53c3e25151d8df
Bret Williams <bretw84@gmail.com>
2020-10-20 20:38:34 -0500

    Working on game logic...need help Spooky bloody eyeball card hover cursor Spidey in the corner toggles terrifying spider animations more UI work

--- js/app.js
+++ js/app.js
@@ -1,40 +1,56 @@
 $(document).ready(function() {
+  
+  var selected = [];
 
-      var pieces = {
-        "icons":[
-          {"icon":"🦇","id":0},
-          {"icon":"🎃","id":1},
-          {"icon":"🧹","id":2},
-          {"icon":"🕷","id":3},
-          {"icon":"🕸","id":4},
-          {"icon":"🧛‍♀️","id":5},
-          {"icon":"💀","id":6},
-          {"icon":"👻","id":7},
-          {"icon":"🦇","id":0},
-          {"icon":"🎃","id":1},
-          {"icon":"🧹","id":2},
-          {"icon":"🕷","id":3},
-          {"icon":"🕸","id":4},
-          {"icon":"🧛‍♀️","id":5},
-          {"icon":"💀","id":6},
-          {"icon":"👻","id":7}
-        ]
-      }
-      
-      pieces.icons.sort(() => .5 - Math.random());
+  var pieces = {
+    "icons":[
+      {"icon":"🦇","id":0},
+      {"icon":"🦇","id":0},
+      {"icon":"🎃","id":1},
+      {"icon":"🎃","id":1},
+      {"icon":"🩸","id":2},
+      {"icon":"🩸","id":2},
+      {"icon":"🕷","id":3},
+      {"icon":"🕷","id":3},
+      {"icon":"🕸","id":4},
+      {"icon":"🕸","id":4},
+      {"icon":"🧛‍♀️","id":5},
+      {"icon":"🧛‍♀️","id":5},
+      {"icon":"💀","id":6},
+      {"icon":"💀","id":6},
+      {"icon":"👻","id":7},
+      {"icon":"👻","id":7}
+    ]
+  }
 
-      // console.log(pieces.icons);
+  pieces.icons.sort(() => .5 - Math.random());
 
-      for(var i = 0; i < pieces.icons.length; i++) {
-        $('.container-fluid .row').append('<div class="col-sm-3"><div id="'+i+'" class="card" style="width: 18rem;"><div class="card-body"><h2 class="text-center"><span class="icon d-none '+pieces.icons[i]['id']+'">'+pieces.icons[i]['icon']+'</span></h2></div></div></div>');
-      }
+  // console.log(pieces.icons);
 
-      $('.card').click(function() {
-       var id = $(this).attr('id');
-       var icon = $('#'+id+' .icon');  
-       icon.toggleClass('d-none');
-       console.log(icon);
+  for(var i = 0; i < pieces.icons.length; i++) {
+    $('.container-fluid .row').append('<div class="col-sm-3"><div id="'+i+'" class="card" style="width: 18rem;"><div class="card-body"><h2 class="text-center"><span class="icon d-none '+pieces.icons[i]['id']+'">'+pieces.icons[i]['icon']+'</span></h2></div></div></div>');
+  }
+
+  $('.card').click(function() {
+    var id = $(this).attr('id');
+    var icon = $('#'+id+' .icon');
+    selected.push(id);
+
+    if(selected.length > 1) {
+      $('.icon').each(function() {
+        if(icon.hasClass('selected')) {
+          icon.removeClass('selected');
+          icon.addClass('d-none');
+        }
+        selected.length = 0;
+        console.log(selected.length);
       });
+    }else {
+      console.log(selected.length);
+      icon.addClass('selected');
+      icon.removeClass('d-none');
+          
+    }
+  });
 
 });
-
