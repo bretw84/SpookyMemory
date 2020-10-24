9c4099202f828881b20c49563fb23bce108a243d
Dakota Schiedermayer <kotaschied23@yahoo.com>
2020-10-24 14:16:36 -0500

    Responsive design. Minimum device size of 576px

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
       icons.sort(() => .5 - Math.random())
 
       for(var i = 0; i < icons.length; i++) {
-        $('.container-fluid .row').append('<div class="col-sm-3"><div class="card" style="width: 18rem;"><div class="card-body"><h2 class="text-center">'+icons[i]+'</h2></div></div></div>');
+        $('.container-fluid .row').append('<div class="columns"><div class="card"><div class="card-body"><h2 class="text-center">'+icons[i]+'</h2></div></div></div>');
       }
 });
 
