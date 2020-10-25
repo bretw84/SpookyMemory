02d36a4fff083c9c6d05895b193f66c2afac6fdf
Bret Williams <bretw84@gmail.com>
2020-10-25 12:58:38 -0500

    Merged dakotas reponsive changes and landing page There were some issues to resolve, and some hooks that were lost that I added back in. Now i have the landing page and the responsive grid, and I still have my show/hide click that needs animation and still need to work out the logic and flow

--- js/app.js
+++ js/app.js
@@ -27,8 +27,8 @@ $(document).ready(function() {
 
   // console.log(pieces.icons);
 
-  for(var i = 0; i < icons.length; i++) {
-        $('.container-fluid .row').append('<div class="columns"><div class="card"><div class="card-body"><h2 class="text-center">'+icons[i]+'</h2></div></div></div>');
+  for(var i = 0; i < pieces.icons.length; i++) {
+        $('.container-fluid .row').append('<div class="columns"><div id="'+i+'" class="card"><div class="card-body"><h2 class="text-center"><span class="icon d-none '+pieces.icons[i]['id']+'">'+pieces.icons[i]['icon']+'</span></h2></div></div></div>');
     }
 
   $('.card').click(function() {
